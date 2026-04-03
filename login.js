12645b57a59d0a17557f788e4550531f3c711198
Avnish-kumar-01 <avnishchauhan7088@gmail.com>
2026-04-04 00:42:53 +0530

    Update login.js

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ loginForm.addEventListener("submit", async (e) => {
     const password = document.getElementById("password").value.trim();
 
     try {
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch(" https://smart-dashboard-3.onrender.com/api/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
